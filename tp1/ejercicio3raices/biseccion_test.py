cf602a1fb64456991d32ef85c0bd0dcdad1b610f
Valen <61921501+valencorrea@users.noreply.github.com>
2020-11-14 00:40:40 -0300

    agrego una biseccion test separada con formato de taller

---
+++ tp1/ejercicio3raices/biseccion_test.py
@@ -0,0 +1,33 @@
+import numpy as np
+import biseccion as bi
+
+def funcion(x):
+
+    return np.sin(x)
+
+print()
+print("Metodo: Biseccion")
+print()
+
+print("Prueba 1") #renombrar prueba
+print()
+
+raices_biseccion = bi.raiz(funcion, np.pi/2, 3*np.pi/2, 0.00001, 20)[0]
+iteracion = 0
+
+for raiz in raices_biseccion:
+    print("Iteracion nº{0}: {1}".format(iteracion, raices_biseccion[iteracion]))
+    iteracion += 1
+
+print()
+print()
+print("Prueba 2") #renombrar prueba
+print()
+
+raices_biseccion = bi.raiz(funcion, 1, 4, 0.00001, 20)[0]
+iteracion = 0
+
+for raiz in raices_biseccion:
+    print("Iteracion nº{0}: {1}".format(iteracion, raices_biseccion[iteracion]))
+    iteracion += 1
+
