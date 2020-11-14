913ce2b91cca90bb42d83b858f737ad5c09c4667
soldiazreal <54005737+soldiazreal@users.noreply.github.com>
2020-11-14 20:25:59 -0300

    Refactor resolucion

--- tp1/ejercicio1candado/resolucion.py
+++ tp1/ejercicio1candado/resolucion.py
@@ -1,25 +1,53 @@
 import random
 import matplotlib.pyplot as plt
-cant_experimentos = 100000
-cant_intentos = [0] * (cant_experimentos)
-cant_combinaciones = 10000
 
-for num_experim in range (0,cant_experimentos-1):
+'''-----------------Punto A-----------------'''
+def definir_clave():
+    cant_combinaciones = 10000
     numero = int(random.random()*(cant_combinaciones-1))
+    print("La clave es:",numero)
+    return numero
+
+'''-----------------Punto B-----------------'''
+def realizar_fuerza_bruta(numero):
     comb_descubierta = False
     comb_a_probar = 0
     while comb_descubierta == False:
         if (comb_a_probar == numero):
             comb_descubierta = True
-            cant_intentos[num_experim] = comb_a_probar + 1
+            print("La clave encontrada es:",comb_a_probar)
         comb_a_probar += 1
 
+def adivinar_clave():
+    numero = definir_clave()
+    realizar_fuerza_bruta(numero)
 
-long_bins = 10
-plt.figure(figsize=(15,8))
-plt.hist(cant_intentos, bins=int(cant_combinaciones/long_bins), range=(0,cant_combinaciones-1))
-plt.title('Histograma de cantidad de intentos realizados para hallar la combinación')
-plt.xlabel('Cantidad de intentos agrupados de 10 en 10')
-plt.show()
+adivinar_clave()
 
+'''-----------------Punto C-----------------'''
+def realizar_experimento():
+
+    cant_experimentos = 100000
+    cant_intentos = [0] * (cant_experimentos)
+    cant_combinaciones = 10000
+
+    for num_experim in range (0,cant_experimentos-1):
+        numero = int(random.random()*(cant_combinaciones-1))
+        comb_descubierta = False
+        comb_a_probar = 0
+        while comb_descubierta == False:
+            if (comb_a_probar == numero):
+                comb_descubierta = True
+                cant_intentos[num_experim] = comb_a_probar + 1
+            comb_a_probar += 1
+    
+
+    long_bins = 10
+    plt.figure(figsize=(15,8))
+    plt.hist(cant_intentos, bins=int(cant_combinaciones/long_bins), range=(0,cant_combinaciones-1))
+    plt.title('Histograma de cantidad de intentos realizados para hallar la combinación')
+    plt.xlabel('Cantidad de intentos agrupados de 10 en 10')
+    plt.show()
+
+realizar_experimento()
 
