2b76378818caab3ca43c76b5f02ff7cc28f0758e
AgussAndrade <51175036+agussandrade@users.noreply.github.com>
2021-03-03 22:21:53 -0300

    cambio graficos

--- tp2/item_b/grafico.py
+++ tp2/item_b/grafico.py
@@ -1,19 +1,26 @@
+import numpy as np
+
 from item_a.resultados import resultados
 import matplotlib.pyplot as plt
+from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 
 t = resultados[:, 0]
 x = resultados[:, 1]
 y = resultados[:, 2]
+fig, ax = plt.subplots(figsize=(10, 8))
 
+ax.set_xlim(0, 30)
+ax.set_ylim(0, 7)
+
+ax.xaxis.set_major_locator(MultipleLocator(2))
+ax.yaxis.set_major_locator(MultipleLocator(1))
+
+ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+
+ax.grid(which='major', color='#CCCCCC', linestyle='--')
+ax.grid(which='minor', color='#CCCCCC', linestyle=':')
 plt.plot(t, x, label='presa')
 plt.plot(t, y, label='depredador')
-
-plt.xlim(0, 30)
-plt.ylim(0, 7)
-
-plt.title('depredador-presa')
-plt.xlabel('tiempo')
-plt.legend(loc='best')
-plt.grid(True)
 plt.savefig("grafico_item_b.jpg")
 plt.show()
